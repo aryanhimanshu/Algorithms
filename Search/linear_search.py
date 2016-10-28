5e90837b9be59fa92d7ced22c7eab1ff52c92d79
Corwin Smith <corwin.smith@uleth.ca>
2016-10-27 23:17:41 -0600

    Added Binary and Linear search for Python

---
+++ Search/linear_search.py
@@ -0,0 +1,8 @@
+# Obj is the object we are searching
+# Item is the thing we are searching the object for
+# Start is the position of the object we wish to start the search of the object on, change start for different position
+def linear_search(obj, item, start=0):
+    for i in range(start, len(obj)):
+        if obj[i] == item:
+            return i
+    return -1
