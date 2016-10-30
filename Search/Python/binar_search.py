f634ba75f891a4bf47902b2432aa266259d874bf
nitikataneja <way2nitika@gmail.com>
2016-10-30 18:58:35 +0530

    extra variable removed

--- Search/Python/binar_search.py
+++ Search/Python/binar_search.py
@@ -1,14 +1,13 @@
 def binary_search(obj, item):
-    found = False
     start = 0
     last = len(obj) - 1
 
     while first <= lsat and not found:
         mid = (first + last) / 2
         if obj[mid] == item:
-            found = True
+            return True
         else:
             if item < obj[mid]:
                 last = mid - 1
             else: first = mid + 1
-    return found
+    return false
