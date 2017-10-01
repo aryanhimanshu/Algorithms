84d4a83cf1cc55c077aaddc945167f796c6f2c88
Maxwell Heeschen <gorpmaster@gmail.com>
2017-10-01 16:45:48 -0500

    Fix tab/spaces mixup. I had mixed spaces with tabs

--- Search/C++/binary.cpp
+++ Search/C++/binary.cpp
@@ -9,22 +9,22 @@ int bsearch(int a[],int x,int i,int j){
 			return 1;
 		}
 		if(a[mid]>x){
-            j=mid-1;
-        }
+				j=mid-1;
+		}
 		if(a[mid]<x){
-            i=mid+1;
-        }
+				i=mid+1;
+		}
 	}
 	return 0;
 }
 main(){
 	int n;
 	a[]={1,2,3,4,5,6,7};
-    n=7;
+	n=7;
 	sort(a,a+n);
 	if(bsearch(a,4,0,6)){
-        cout<<"prsent"<<endl;
-    }
-    else
-        cout<<"not found"<<endl;
+		  cout<<"prsent"<<endl;
+	 }
+	 else
+		  cout<<"not found"<<endl;
 }
