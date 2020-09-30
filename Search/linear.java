bcc58039331ed9d872ecb7101ccd37eb7b70f711
Abhi Aryan <64599900+abhi007aryan@users.noreply.github.com>
2020-09-30 22:28:58 +0530

    Update linear.java

--- Search/linear.java
+++ Search/linear.java
@@ -5,13 +5,14 @@ public class linearsearch {
 	//this is the linear function that takes input array and finds the element and prints outpu accordingly
 	public static void linear(int[] arr,int x){
 		
-		int n=arr.length,index=0,flag=0;
+		int n=arr.length,index=0;
+		boolean flag=false;
 		
 		//main for loop where the array is traversed and in case element is found a variable called flag is used to indicate so.
 		//Also the position of variable is stored in a variable index.
 		for(int i=0;i<n;i++){
 			if(arr[i]==x){
-				flag=1;
+				flag=true;
 				index=i;
 				break;
 			}
@@ -19,7 +20,7 @@ public class linearsearch {
 		
 		//finally if the flag indicates that the variable was found during traversal,the value of index is printed otherwise element not found is printed.
 		//NOTE:In java arrays are traditionally zero indexed and that has been used in this code.
-		if(flag==1){
+		if(flag){
 			System.out.println(x + " was found at position "+ index);
 		}
 		else{
@@ -38,7 +39,7 @@ public class linearsearch {
 		
 		n=sc.nextInt();
 		int[] arr=new int[n];
-		
+	
 		System.out.println("Enter the numbers for the array");
 		
 		for(int i=0;i<n;++i){
